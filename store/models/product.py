f05a04821be6ada8cb4baf9cd7a5a7572a775cf8
Hridoy <hriodykumarshil12@gmail.com>
2023-08-02 13:02:42 +0600

    Creating Product From Admin Site

--- store/models/product.py
+++ store/models/product.py
@@ -4,5 +4,5 @@ class Product(models.Model):
     Name= models.CharField(max_length=50)
     Price= models.IntegerField(default=0)
     Description= models.CharField(max_length=200, default='')
-    Image= models.ImageField(upload_to='products/')
+    Image= models.ImageField(upload_to='uploads/products/')
 
