07ff07e71cb631889bc5813b9a07971a8b2681e6
Hridoy <hriodykumarshil12@gmail.com>
2023-08-04 12:27:25 +0600

    Getting Products Home page
    https://docs.djangoproject.com/en/4.2/topics/db/queries/#retrieving-all-objects
    
    https://docs.djangoproject.com/en/4.2/ref/templates/language/

--- store/models/product.py
+++ store/models/product.py
@@ -6,5 +6,11 @@ class Product(models.Model):
     Name = models.CharField(max_length=50)
     Price = models.IntegerField(default=0)
     Category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
-    Description = models.CharField(max_length=200, default='')
+    Description = models.CharField(max_length=200, default='', null=True, blank=True)
     Image = models.ImageField(upload_to='uploads/products/')
+
+
+    @staticmethod
+    def get_all_products():
+        return Product.objects.all()
+
--- store/views.py
+++ store/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.http import  HttpResponse
+from django.http import HttpResponse
+from .models.product import Product
 
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'orders/orders.html')
+    Products = Product.get_all_products();
+    return render(request, 'index.html', {'Products': Products})
