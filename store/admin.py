4d2dfa0bc8f33d74246f98eacb25d63a48c11cf0
Hridoy <hriodykumarshil12@gmail.com>
2023-08-04 10:11:58 +0600

    Showing Table View In Admin Site

--- store/admin.py
+++ store/admin.py
@@ -3,7 +3,15 @@ from .models.product import Product
 from .models.category import Category
 
 
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['Name', 'Price', 'Category', 'Image']
+
+
+class AdminCategory(admin.ModelAdmin):
+    list_display = ['Name']
+
+
 # Register your models here.
 
-admin.site.register(Product)
-admin.site.register(Category)
+admin.site.register(Product, AdminProduct)
+admin.site.register(Category, AdminCategory)
--- store/models/category.py
+++ store/models/category.py
@@ -3,3 +3,6 @@ from django.db import models
 
 class Category(models.Model):
     Name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.Name
