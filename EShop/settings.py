bef822646042b950717bac3f540310ee8fe09245
Hridoy <hriodykumarshil12@gmail.com>
2023-08-17 23:21:59 +0600

    Middleware
    https://docs.djangoproject.com/en/4.2/topics/http/middleware/
    
    Activating middleware
    
    https://www.youtube.com/watch?v=pb1Icz69yMY&list=PLdBwVRHjcI__NWxctXUSLz1Gg2Mb-B-O-&index=56

--- EShop/settings.py
+++ EShop/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import store.middlewares.auth
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -48,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'store.middlewares.auth.auth_middleware',
 ]
 
 ROOT_URLCONF = 'EShop.urls'
--- store/views/orders.py
+++ store/views/orders.py
@@ -7,11 +7,15 @@ from store.models.category import Category
 from store.models.customer import Customer
 from django.views import View
 from store.models.orders import Order
+from store.middlewares.auth import auth_middleware
+from django.utils.decorators import method_decorator
+
 
 class OrderView(View):
+    @method_decorator(auth_middleware)
     def get(self, request):
-        customer= request.session.get('customer')
-        orders= Order.get_orders_by_customer(customer)
+        customer = request.session.get('customer')
+        orders = Order.get_orders_by_customer(customer)
         # orders: orders.reverse()
         print(orders)
         return render(request, 'orders.html', {'orders': orders})
